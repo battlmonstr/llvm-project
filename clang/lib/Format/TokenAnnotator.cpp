a454fbb70314b2ef55434d5e6e21d97e38897cf1
battlmonstr <battlmonstr@users.noreply.github.com>
2019-08-04 19:39:37 +0200

    space before "const" for left-aligned pointers
    Before:
    NSString *const kFoo = @"bar";
    
    After:
    NSString * const kFoo = @"bar";

--- clang/lib/Format/TokenAnnotator.cpp
+++ clang/lib/Format/TokenAnnotator.cpp
@@ -2475,6 +2475,7 @@ bool TokenAnnotator::spaceRequiredBetween(const AnnotatedLine &Line,
     return Right.Tok.isLiteral() || Right.is(TT_BlockComment) ||
            (Right.isOneOf(Keywords.kw_override, Keywords.kw_final) &&
             !Right.is(TT_StartOfName)) ||
+           Right.is(tok::kw_const) ||
            (Right.is(tok::l_brace) && Right.BlockKind == BK_Block) ||
            (!Right.isOneOf(TT_PointerOrReference, TT_ArraySubscriptLSquare,
                            tok::l_paren) &&
